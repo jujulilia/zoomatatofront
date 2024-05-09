a3081fba3ac45d56bb391a4704712acf69d0fde3
jujulilia <jujuscosta07@gmail.com>
2024-05-09 15:45:55 -0300

    Atualização

---
+++ src/interfaces/animalInterface.tsx
@@ -0,0 +1,12 @@
+interface Animal{
+    id: number;
+    nome: string;
+    especie: string;
+    peso: number;
+    altura: number;
+    sexo: string;
+    dieta: string;
+    habitat: string;
+    idade: string;
+    ra: string;
+}
