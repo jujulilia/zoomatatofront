a73b1c99c880bf757ad2d0adf1d2cefb340dfc5c
jujulilia <jujuscosta07@gmail.com>
2024-05-21 15:45:04 -0300

    21/05

--- src/screens/CadastroAnimais.tsx
+++ src/screens/CadastroAnimais.tsx
@@ -2,17 +2,17 @@ import React, { useState } from "react";
 import { Image, StatusBar, StyleSheet, Text, TextInput, TouchableOpacity, View, ScrollView } from "react-native";
 import axios from 'axios';
 
-const CadastroAnimal = () => {
-    const [animais, setAnimais] = useState<Animal[]>([]);
-    const [nome, setNome] = useState<string>('');
-    const [idade, setIdade] = useState<string>('');
-    const [especie, setEspecie] = useState<string>('');
-    const [ra, setRa] = useState<string>('');
-    const [peso, setPeso] = useState<string>('');
-    const [altura, setAltura] = useState<string>('');
-    const [sexo, setSexo] = useState<string>('');
-    const [dieta, setDieta] = useState<string>('');
-    const [habitat, setHabitat] = useState<string>('');
+const CadastroAnimais = () => {
+    const [animais, setAnimais] = useState([]);
+    const [nome, setNome] = useState('');
+    const [idade, setIdade] = useState('');
+    const [especie, setEspecie] = useState('');
+    const [ra, setRa] = useState('');
+    const [peso, setPeso] = useState('');
+    const [altura, setAltura] = useState('');
+    const [sexo, setSexo] = useState('');
+    const [dieta, setDieta] = useState('');
+    const [habitat, setHabitat] = useState('');
 
     const logo = require('../assets/images/logo.png');
 
@@ -24,12 +24,12 @@ const CadastroAnimal = () => {
             formData.append('especie', especie);
             formData.append('ra', ra);
             formData.append('peso', peso);
-            formData.append('altura',altura);
+            formData.append('altura', altura);
             formData.append('sexo', sexo);
             formData.append('dieta', dieta);
             formData.append('habitat', habitat);
-           
-            const response = await axios.post('http://10.137.11.225:8000/api/animal/cadastrar', formData, {
+
+            const response = await axios.post('http://10.137.11.227:8000/api/animal/cadastrar', formData, {
                 headers: {
                     'Content-Type': 'multipart/form-data'
                 }
@@ -39,7 +39,6 @@ const CadastroAnimal = () => {
             console.log(error);
         }
     }
-        
 
     return (
         <ScrollView style={styles.container}>
@@ -47,92 +46,107 @@ const CadastroAnimal = () => {
             <View style={styles.header}>
                 <Image source={logo} style={styles.logo} />
             </View>
+
             <View style={styles.form}>
                 <Text style={styles.fText}>Cadastro de Animais</Text>
                 <TextInput
                     style={styles.input}
                     placeholder="Nome do Animal"
-                    value={nome}
                     onChangeText={setNome} />
                 <TextInput
                     style={styles.input}
                     placeholder="Idade"
-                    value={String(idade)}
                     onChangeText={setIdade} />
                 <TextInput
                     style={styles.input}
                     placeholder="Espécie"
-                    value={especie}
                     onChangeText={setEspecie} />
                 <TextInput
                     style={styles.input}
                     placeholder="RA"
-                    value={ra}
                     onChangeText={setRa} />
                 <TextInput
                     style={styles.input}
                     placeholder="Peso"
-                    value={peso}
                     onChangeText={setPeso} />
                 <TextInput
                     style={styles.input}
                     placeholder="Altura"
-                    value={(altura)}
                     onChangeText={setAltura} />
                 <TextInput
                     style={styles.input}
                     placeholder="Sexo"
-                    value={sexo}
                     onChangeText={setSexo} />
                 <TextInput
                     style={styles.input}
                     placeholder="Dieta"
-                    value={dieta}
                     onChangeText={setDieta} />
                 <TextInput
                     style={styles.input}
                     placeholder="Habitat"
-                    value={habitat}
                     onChangeText={setHabitat} />
-               
-                <TouchableOpacity style={styles.imageButton} onPress={cadastrarAnimal}>
-                    <Text style={styles.imageButtonText}>Cadastrar Animal</Text>
+            </View>
+
+            <TouchableOpacity style={styles.imageButton} onPress={cadastrarAnimal}>
+                <Text style={styles.imageButtonText}>Cadastrar Animal</Text>
+            </TouchableOpacity>
+
+            <View style={styles.footer}>
+                <TouchableOpacity>
+                    <Image
+                        source={require('../assets/images/home.png')}
+                        style={styles.footerIcon}
+                    />
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <Image
+                        source={require('../assets/images/profile.png')}
+                        style={styles.footerIcon}
+                    />
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <Image
+                        source={require('../assets/images/menu.png')}
+                        style={styles.footerIcon}
+                    />
                 </TouchableOpacity>
             </View>
         </ScrollView>
     );
 }
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
     },
     header: {
-        backgroundColor: '#ffdab9',
+        backgroundColor: '#606c38',
         paddingVertical: 10,
         alignItems: 'center',
     },
     logo: {
-        width: 130,
-        height: 100,
+        width: 200,
+        height: 130,
     },
     form: {
         padding: 10,
-        backgroundColor: 'white',
+        backgroundColor: '#ccd5ae',
         marginBottom: 10,
         paddingVertical: 30,
     },
     input: {
         height: 40,
-        borderColor: 'gray',
+        borderColor: 'white',
         borderWidth: 1,
         marginBottom: 10,
         paddingHorizontal: 10,
         borderRadius: 10,
+        backgroundColor: '#fefae0'
     },
     imageButton: {
-        backgroundColor: '#ffdab9',
+        backgroundColor: '#606c38',
         padding: 10,
-        borderRadius: 5,
+        borderRadius: 20,
         alignItems: 'center',
         marginBottom: 10,
     },
@@ -144,21 +158,32 @@ const styles = StyleSheet.create({
         fontSize: 20,
         fontWeight: 'bold',
         color: 'black',
-        paddingVertical: 10,
+        paddingVertical: 12,
+        paddingHorizontal: 'auto'
     },
     footer: {
         borderTopWidth: 1,
-        backgroundColor: 'white',
+        backgroundColor: '#fefae0',
         flexDirection: 'row',
         justifyContent: 'space-around',
         alignItems: 'center',
         paddingVertical: 20,
+        paddingHorizontal: 20,
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5
     },
+
     footerIcon: {
-        width: 30,
-        height: 30,
+        width: 24,
+        height: 24,
+        margin: 10,
     },
 });
 
-
-export default CadastroAnimal;
+export default CadastroAnimais;
---
+++ src/screens/ListagemAnimais.tsx
@@ -0,0 +1,129 @@
+import React, { useState, useEffect } from 'react';
+import { FlatList, Image, StatusBar, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import axios from 'axios';
+
+interface Animal {
+    id: string;
+    nome: string;
+    especie: string;
+    idade: string;
+    ra: string;
+    peso: string;
+    altura: string;
+    sexo: string;
+    dieta: string;
+    habitat: string;
+}
+
+const ListagemAnimais: React.FC = () => {
+    const [animais, setAnimais] = useState<Animal[]>([]);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        const fetchAnimais = async () => {
+            try {
+                const response = await axios.get('http://10.137.11.227:8000/api/animal/todos');
+                setAnimais(response.data.data);
+                console.log(response.data.data)
+            } catch (error) {
+                setError("Ocorreu um erro ao buscar os animais");
+            }
+        };
+
+        fetchAnimais();
+    }, []);
+
+    const renderItem = ({ item }: { item: Animal }) => (
+        <View style={styles.itemContainer}>
+            <TouchableOpacity style={styles.item}>
+                <View style={styles.text}>
+                    <Text style={styles.tituloAnimais}>{item.nome}</Text>
+                    <Text style={styles.especie}>{item.especie}</Text>
+                    <Text style={styles.idade}>{item.idade}</Text>
+                </View>
+            </TouchableOpacity>
+        </View>
+    );
+
+    return (
+        <View style={styles.container}>
+            <StatusBar backgroundColor="black" barStyle='light-content' />
+            <View style={styles.header}>
+                <Text style={styles.headerText}>Lista de Animais Cadastrados</Text>
+            </View>
+
+            <FlatList
+                data={animais}
+                renderItem={({ item }) => renderItem({ item })}
+                keyExtractor={(item) => item.id.toString()}
+            />
+        </View>
+    );
+};
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    header: {
+        backgroundColor: '#ffdab9',
+        alignItems: 'center',
+        paddingVertical: 20
+    },
+    headerText: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: 'black'
+    },
+    itemContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginVertical: 8,
+        marginHorizontal: 8,
+    },
+    item: {
+        backgroundColor: '#feedc6',
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 8,
+        borderRadius: 20,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    image: {
+        width: 130,
+        height: 100,
+        marginRight: 25
+    },
+    text: {
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+    },
+    tituloAnimais: {
+        fontWeight: 'bold',
+        color: 'black',
+        fontSize: 20,
+        textAlign: 'center',
+        marginBottom: 10,
+    },
+    especie: {
+        fontWeight: 'bold',
+        color: 'black',
+        fontSize: 16,
+        marginBottom: 10,
+    },
+    idade: {
+        fontWeight: 'bold',
+        color: 'black',
+        fontSize: 16,
+    },
+
+});
+
+export default ListagemAnimais;
+
